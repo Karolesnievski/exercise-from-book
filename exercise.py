3829e775df0f8b85c32c7fcb4ac6c929260c403b
Karol Leśniewski <lesiulesniewski@gmail.com>
2023-01-26 22:07:06 +0100

    second exercise was done

--- exercise.py
+++ exercise.py
@@ -7,3 +7,11 @@ def sth (x):
         print("huhuuhuh")
 sth(10)
 
+def hts (x):
+    if x <= 10:
+        print("mniejsza")
+    elif x > 10 and x <= 25:
+        print("mosze") 
+    elif x > 25:
+        print("giga") 
+hts(11)
